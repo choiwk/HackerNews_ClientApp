0edcc43b1b796afe727f5c476659644f50333b24
wonkeunChoi <77766718+choiwk@users.noreply.github.com>
2022-07-06 00:09:52 +0900

    ⚡️ 링크 페이지 리팩토링 - 문자열 -> 함수화 및 라우터 처리

--- app.js
+++ app.js
@@ -2,34 +2,51 @@ const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
-const div = document.createElement('div');
 
 const getData = (url) => {
   ajax.open('GET', url, false);
   ajax.send();
-  console.log(JSON.parse(ajax.response));
+
   return JSON.parse(ajax.response);
 };
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
-window.addEventListener('hashchange', () => {
+const newsDetail = () => {
   const id = location.hash.substring(1);
   const newContents = getData(CONTENT_URL.replace('@id', id));
 
-  const h1 = document.createElement('h1');
-  h1.innerHTML = newContents.title;
-  div.appendChild(h1);
-});
-newsFeed.map((el, idx) => {
-  const div = document.createElement('div');
+  container.innerHTML = `
+ <h1>${newContents.title}</h1>
+ <div>
+ <a href=''>목록으로</a>
+ </div>
+ `;
+};
 
-  div.innerHTML = `<li>
+const newsFeed = () => {
+  const newsFeedData = getData(NEWS_URL);
+  const newList = [];
+
+  newList.push('<ul>');
+  newsFeedData.map((el, idx) => {
+    newList.push(`<li>
   <a href='#${el.id}'>${el.title} (${el.comments_count})</a>
-  </li>`;
+  </li>`);
+  });
+  newList.push('</ul>');
 
-  ul.appendChild(div.firstElementChild);
-});
+  container.innerHTML = newList.join('');
+};
 
-container.appendChild(ul);
-container.appendChild(div);
+const router = () => {
+  const routePath = location.hash;
+  console.log(routePath);
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+};
+
+window.addEventListener('hashchange', router);
+
+router();
